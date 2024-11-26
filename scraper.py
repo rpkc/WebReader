77edf0afbbcea897574c0b45cfb7e95d00b1db9b
rupak <rupakc770@gmail.com>
2024-11-26 09:23:10 +0530

    Change in README

---
+++ scraper.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+class GoogleFinance:
+
+    def __init__(self,symbol:str):
+        __code:str=symbol.upper().strip()
+        url:str="https://www.google.com/finance/quote/"+__code
+        result=requests.get(url)
+        self.parsed_page=BeautifulSoup(result.text, 'html.parser')
+        self.details=dict()
+
+    def __filter(self,value:str)->str:
+        value=value.replace('₹','')
+        value=value.replace(',','')
+        return float(value.strip())
+
+    def __scrapper(self,class_:str)->str:
+        result = self.parsed_page.find(class_=class_).get_text()    
+        return result
+
+    def __scrapper_all(self,class_:str)->list:
+        result = self.parsed_page.find_all(class_=class_)
+        return result
+
+    def price(self)->float:
+        try:
+            return self.__filter(self.__scrapper("YMlKec fxKbKc"))
+        except:
+            return 0.0
+
+    def get_details(self)->dict:
+        results = self.__scrapper_all("P6K39c")
+        day_range=results[1].get_text()
+        year_range=results[2].get_text()
+        self.details={
+            'name':self.__scrapper('zzDege'),
+            'price':self.price(),
+            'low':self.__filter(day_range.split('-')[0]),
+            'high':self.__filter(day_range.split('-')[1]),
+            'low52':self.__filter(year_range.split('-')[0]),
+            'high52':self.__filter(year_range.split('-')[1]),
+            'pe':float(results[5].get_text())
+            }
+        return self.details    
+
+
